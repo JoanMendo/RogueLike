25b913f76a6b5577a5a05eede0c579c02e48bc00
Joan Mendo <joan.mendo.7e7@itb.cat>
2024-12-07 15:47:00 +0100

    sigo jugando al isaac

--- Assets/Script/Weapons/Proyectile.cs
+++ Assets/Script/Weapons/Proyectile.cs
@@ -9,6 +9,7 @@ public class Proyectile : AWeapon
     public float Speed;
     public GameObject deathParticle;
     private Rigidbody2D rb;
+    public GameObject Shadow;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,6 +19,10 @@ public class Proyectile : AWeapon
     private void Start()
     {
         transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg);
+        GameObject shadow = Instantiate(Shadow, new Vector3(transform.position.x, transform.position.y -1, transform.position.x), transform.rotation, transform);
+        //Make the y bigger if the rotation is either 90 or 270, and smaller if it is 0 or 180. The min is 1, and for each degree between the lower and the max the width gets a bit bigger.
+        shadow.transform.localScale = new Vector3(2f, 1f + Mathf.Abs(Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.Deg2Rad)), 1);
+
     }
 
     // Update is called once per frame
